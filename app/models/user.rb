7c2f53e5abc5f2507f20219f71f6f0a2c7369f2c
Tra Claude <vroclaude@gmail.com>
2017-01-15 21:43:21 +0100

    Upadte loggin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,28 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
+  validate :email_is_unique, on: :create
+  after_create :create_account
 
   def confirmation_required?
     false
+  end
+
+
+
+  private
+
+  def email_is_unique
+    #logger.debug "################## dans la fonction email_is_unique ################# "
+    return false unless self.errors[:email].empty?
+    unless Account.find_by_email(email).nil?
+      errors.add(:email, " already exist ")
+    end
+  end
+
+  def create_account
+    account = Account.new(:email => email)
+    account.save!
   end
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170115173529) do
+ActiveRecord::Schema.define(version: 20170115185437) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
