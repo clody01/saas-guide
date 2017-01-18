7d2f895af4bd88c2376de9a60ce1bcc7e2d2308e
clody01 <tseoue01@gmail.com>
2017-01-18 14:28:38 +0100

    configured Gmail delivered

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,10 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  ############################################################################
+  #configuration for devise
+  config.action_mailer.default_url_options = { :host=> 'localhost:3000' }
+  ############################################################################
+
 end
